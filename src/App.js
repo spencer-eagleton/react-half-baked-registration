5e2ba2768a3e0e2634182cf979f563b4871a83ef
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-10 20:39:07 -0800

    fixed types. broke sign up?

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import { NavLink, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import SignIn from './Views/SignIn';
+// import SignIn from './Views/SignIn';
+// import SignUp from './Views/SignUp';
+import { useState } from 'react';
 import './App.css';
-import SignUp from './Views/SignUp';
+import { getUser, logout } from './services/users';
+import Auth from './Views/Auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
   return (
     <div className="App">
       <BrowserRouter>
-        <NavLink to="/signin">Sign In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
         <Switch>
-          <Route path="/signin" exact>
+          {/* <Route path="/signin" exact>
             <SignIn />
           </Route>
           <Route path="/signup" exact>
             <SignUp />
+          </Route> */}
+          <Route path="/" exact>
+            {currentUser && (
+              <>
+                <h1>I am signed in</h1>
+                <button onClick={logoutUser}>Log Out</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+          <Route>
+            <Auth />
           </Route>
         </Switch>
       </BrowserRouter>
